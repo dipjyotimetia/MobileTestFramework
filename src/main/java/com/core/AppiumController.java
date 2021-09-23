a06eee3dff2aa26c31d488258431571fe77dbcef
Dipjyoti Metia <dipjyotimetia@gmail.com>
2021-09-23 23:10:29 +1000

    updated url

--- src/main/java/com/core/AppiumController.java
+++ src/main/java/com/core/AppiumController.java
@@ -259,21 +259,21 @@ public class AppiumController implements Access {
                 _caps.setCapability("browserName", "Chrome");
                 _caps.setCapability("appium:deviceName", "Android GoogleAPI Emulator");
                 _caps.setCapability("appium:platformVersion", "11.0");
-                _caps.setCapability("app", "storage:filename=Android.SauceLabs.Mobile.Sample.app.2.7.1.apk");
+                _caps.setCapability("app", "https://github.com/saucelabs/sample-app-mobile/releases/download/2.7.1/Android.SauceLabs.Mobile.Sample.app.2.7.1.apk");
                 break;
             case "pixel":
                 _caps.setCapability("platformName", "Android");
                 _caps.setCapability("browserName", "Chrome");
                 _caps.setCapability("appium:deviceName", "Google Pixel 3a GoogleAPI Emulator");
                 _caps.setCapability("appium:platformVersion", "11.0");
-                _caps.setCapability("app", "storage:filename=Android.SauceLabs.Mobile.Sample.app.2.7.1.apk");
+                _caps.setCapability("app", "https://github.com/saucelabs/sample-app-mobile/releases/download/2.7.1/Android.SauceLabs.Mobile.Sample.app.2.7.1.apk");
                 break;
             case "iPhone12":
                 _caps.setCapability("platformName", "iOS");
                 _caps.setCapability("browserName", "Safari");
                 _caps.setCapability("appium:deviceName", "iPhone 12 Pro Simulator");
                 _caps.setCapability("appium:platformVersion", "14.5");
-                _caps.setCapability("app", "storage:filename=iOS.RealDevice.SauceLabs.Mobile.Sample.app.2.7.1.ipa");
+                _caps.setCapability("app", "https://github.com/saucelabs/sample-app-mobile/releases/download/2.7.1/iOS.RealDevice.SauceLabs.Mobile.Sample.app.2.7.1.ipa");
                 break;
             default:
                 System.out.println("No device found");
